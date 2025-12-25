98c78a28622decec64a1a2d757fcb3cfaa279888
myungeun.lee <myungeun.lee@hnine.com>
2025-12-25 15:06:27 +0900

    edit card

--- src/components/public/AccountCard.vue
+++ src/components/public/AccountCard.vue
@@ -1,9 +1,10 @@
 <script setup>
   import { ref } from 'vue'
-  import '@/assets/fonts.css'
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   import { library } from '@fortawesome/fontawesome-svg-core'
   import { faCopy } from '@fortawesome/free-regular-svg-icons'
+  import GroomImg from '@/assets/photo/wedding/groom.png'
+  import BrideImg from '@/assets/photo/wedding/bride.png'
 
   library.add(faCopy)
 
@@ -11,60 +12,62 @@
     {
       type: '신랑측',
       color: '#cfe2ff',
+      sub_color: '#D9E5FF',
       members: [
         {
           role: '아버지',
           name: '박승준',
-          phone: '010-4536-3025',
           bank: '농협',
           account: '352-1733-2958-23',
+          img: GroomImg,
         },
         {
           role: '어머니',
           name: '김미자',
-          phone: '010-5493-3025',
           bank: '농협',
           account: '957-12-375313',
+          img: GroomImg,
         },
         {
           role: '신랑',
           name: '박준영',
-          phone: '010-4094-3541',
           bank: '농협',
           account: '901044-56-072991',
+          img: GroomImg,
         },
       ],
     },
     {
       type: '신부측',
       color: '#f6dce3',
+      sub_color: '#FFEBFE',
       members: [
         {
           role: '아버지',
           name: '이승준',
-          phone: '010-3399-7908',
           bank: '국민',
           account: '038701-04-577887',
+          img: BrideImg,
         },
         {
           role: '어머니',
           name: '강미숙',
-          phone: '010-8982-9234',
           bank: '농협',
           account: '018-12-180860',
+          img: BrideImg,
         },
         {
           role: '신부',
           name: '이명은',
-          phone: '010-9908-9234',
           bank: '국민',
           account: '038701-04-476922',
+          img: BrideImg,
         },
       ],
     },
   ]
-  const openGroup = ref(null)
 
+  const openGroup = ref(null)
   const toggleGroup = (type) => {
     openGroup.value = openGroup.value === type ? null : type
   }
@@ -74,28 +77,26 @@
       await navigator.clipboard.writeText(`${bank} ${account}`)
       alert('계좌번호가 복사되었습니다!')
     } catch {
-      alert('복사에 실패했습니다 😢')
+      alert('복사 실패 😢')
     }
   }
 </script>
+
 <template>
-  <div class="ac_text">
-    참석이 어려우신 분들을 위해 기재했습니다<br />
-    너그러운 마음으로 양해 부탁드립니다
-  </div>
   <div class="family-section">
     <div v-for="group in familyData" :key="group.type" class="family-group">
-      <!-- 그룹 토글 버튼 -->
+      <!-- 토글 버튼 -->
       <button
         class="group-btn"
         :style="{ background: group.color }"
+        :class="{ active: openGroup === group.type }"
         @click="toggleGroup(group.type)"
       >
         {{ group.type }}
         <span>{{ openGroup === group.type ? '▲' : '▼' }}</span>
       </button>
 
-      <!-- 그룹 내용, 스르륵 애니메이션 -->
+      <!-- 열린 그룹 영역 -->
       <transition name="slide">
         <div
           v-if="openGroup === group.type"
@@ -103,21 +104,20 @@
           :style="{ background: group.color }"
         >
           <div v-for="member in group.members" :key="member.name" class="member-card">
+            <div class="member-img">
+              <img :src="member.img" alt="" />
+            </div>
             <div class="member-info">
-              <div class="role_box">
-                <strong>{{ member.role }}</strong>
+              <div class="role-name">
+                <strong>{{ member.role }}</strong> . {{ member.name }}
               </div>
-              <div class="mem_info_box">
-                <div>{{ member.name }}</div>
-                <div class="member-account">
-                  {{ member.bank }} {{ member.account }}
-                  <button class="copy-btn" @click="copyAccount(member.bank, member.account)">
-                    <FontAwesomeIcon icon="fa-regular fa-copy" class="copy_icon" />
-                  </button>
-                </div>
+              <div class="account">
+                {{ member.bank }} {{ member.account }}
+                <button class="copy-btn" @click="copyAccount(member.bank, member.account)">
+                  <FontAwesomeIcon icon="fa-regular fa-copy" />
+                </button>
               </div>
             </div>
-            <!-- <div class="member-phone">{{ member.phone }}</div> -->
           </div>
         </div>
       </transition>
@@ -132,58 +132,75 @@
     font-family: 'KoPubWorld', sans-serif;
   }
 
+  .family-group {
+    margin-bottom: 16px; /* 신랑/신부 그룹 간격 */
+  }
+
+  /* 토글 버튼 */
   .group-btn {
     width: 100%;
     padding: 12px 16px;
     border: none;
-    border-radius: 8px;
+    border-radius: 8px; /* 닫혀 있을 때 둥글게 */
     text-align: left;
     font-weight: 600;
     cursor: pointer;
-    margin-bottom: 8px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    color: #fff;
+    color: #3f3530;
+    margin-bottom: 0;
+    transition: border-radius 0.2s;
   }
 
+  /* 클릭된 상태 */
+  .group-btn.active {
+    border-radius: 8px 8px 0 0;
+    border-bottom: 1px solid #8c8c8c; /* 열린 토글 밑에 선 추가 */
+  }
+
+  /* 열린 그룹 영역 */
   .group-content {
-    padding: 8px 16px;
-    border-radius: 8px;
-    margin-bottom: 16px;
-    background: #fff8f0;
-    overflow: hidden;
+    border-radius: 0 0 10px 10px;
+    padding: 16px;
   }
 
+  /* 인물 카드 */
   .member-card {
-    padding: 8px 0;
-    border-bottom: 1px solid #eee;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    padding: 12px;
+    border-radius: 8px;
+    margin-bottom: 12px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.08);
   }
 
   .member-card:last-child {
-    border-bottom: none;
+    margin-bottom: 0;
+  }
+
+  .member-img img {
+    width: 60px;
+    height: 60px;
+    border-radius: 8px;
+    object-fit: cover;
+    margin-right: 12px;
   }
 
   .member-info {
-    font-weight: 500;
-    margin-bottom: 4px;
-    display: flex;
+    flex: 1;
   }
-  .role_box {
-    width: 20%;
-    text-align: left;
-    height: auto;
-    vertical-align: middle;
+
+  .role-name {
+    font-weight: 600;
+    margin-bottom: 6px;
+  }
+
+  .account {
     display: flex;
-    justify-content: center; /* 가로 중앙 */
     align-items: center;
-  }
-  .mem_info_box {
-    width: 80%;
-    text-align: right;
-  }
-  .member-account {
-    margin-bottom: 4px;
+    gap: 6px;
     font-size: 14px;
   }
 
@@ -193,12 +210,7 @@
     cursor: pointer;
   }
 
-  .member-phone {
-    font-size: 13px;
-    color: #555;
-  }
-
-  /* 스르륵 애니메이션 */
+  /* 슬라이드 애니메이션 */
   .slide-enter-active,
   .slide-leave-active {
     transition: all 0.3s ease;
@@ -213,17 +225,6 @@
   .slide-leave-from {
     max-height: 500px;
     opacity: 1;
-    padding: 8px 16px;
-  }
-  .ac_text {
-    text-align: center;
-    font-size: 14px;
-    letter-spacing: 0.08em;
-    line-height: 2.1;
-    margin: 30px 0;
-    font-family: 'ko_r', sans-serif;
-  }
-  .copy_icon {
-    width: 20px;
+    padding: 16px;
   }
 </style>
