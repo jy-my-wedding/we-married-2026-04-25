204ffc5ca9384c9419a39d1103c5958c06a6bd70
myungeun.lee <myungeun.lee@hnine.com>
2026-02-09 11:54:43 +0900

    노티 위치 수정

--- src/components/public/Reception.vue
+++ src/components/public/Reception.vue
@@ -18,7 +18,21 @@
 
 <template>
   <section class="info-section">
+    <div class="parking-notice-classic">
+      <p class="parking-caption">
+        <span> Notice</span><br />
+        결혼식과 피로연은 각각 다른 날짜와 장소에서 진행됩니다.<br />
+        혼동 없으시도록 일정과 장소를 꼭 확인 부탁드립니다.<br />
+        <br />
+        예식 참석이 어려우신 분들께서는<br />
+        피로연 자리에서 따뜻한 축하로 함께해 주시면<br />
+        큰 기쁨이 되겠습니다.<br />
+      </p>
+    </div>
+    <br />
+    <br />
     <p>2026년 4월 11일 토요일 11:00 ~ 18:00</p>
+    <br />
     <br />
     <p>청우 가든</p>
     <p>
@@ -32,17 +46,6 @@
     <div class="map-content">
       <Chungwoo />
     </div>
-  </div>
-  <div class="parking-notice-classic">
-    <p class="parking-caption">
-      <span> Notice</span><br />
-      결혼식과 피로연은 각각 다른 날짜와 장소에서 진행됩니다.<br />
-      혼동 없으시도록 일정과 장소를 꼭 확인 부탁드립니다.<br />
-      <br />
-      예식 참석이 어려우신 분들께서는<br />
-      피로연 자리에서 따뜻한 축하로 함께해 주시면<br />
-      큰 기쁨이 되겠습니다.<br />
-    </p>
   </div>
 </template>
 
@@ -93,32 +96,8 @@
     size: 30px;
     margin-right: 10px;
   }
-  .info-table {
-    table-layout: fixed;
-    padding: 0 10px;
-    font-size: 13px;
-  }
-  .info-table td {
-    vertical-align: top;
-    height: 25px;
-    line-height: 1.6;
-  }
-  .info-table .b_t {
-    font-weight: bold;
-  }
-  .info-table td:nth-child(1) {
-    width: 50px;
-    font-weight: bold;
-  }
-  .info-table td:nth-child(2) {
-    width: 20px;
-  }
-  .info-table td:nth-child(3) {
-    width: auto;
-  }
 
   .parking-caption {
-    margin-top: 50px;
     text-align: center;
 
     font-size: 13px;
